aa83f95ffe7aad28a295f797c7f1326fe9be58cb
Furkan PINAR <106382856+furkaanpnr@users.noreply.github.com>
2022-11-02 16:33:08 +0300

    Delete Encoder directory

--- Encoder/Constant.py
+++
@@ -1,3 +0,0 @@
-class const:
-    usernameHash = "60562eb61e2bac4f17460fa0dad443d7e8db6e61ba98dbbb954872c4d7b34bb5" #furkan encoded with SHA256
-    passwordHash = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4" #1234 encoded with SHA256
